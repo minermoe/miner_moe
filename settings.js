9e7ffa98c9a95995f919ba284acd26c6f8c7aacb
Miner Moe <miner.moe.1234567890@gmail.com>
2017-12-29 21:36:12 -0500

    move keepalive to settings

--- settings.js
+++ settings.js
@@ -2,6 +2,7 @@ const baseUrl = 'http://miner-mo.bitballoon.com';
 
 module.exports = {
   test: {
+    keepalive: 30 * 1000,
     timeout: 2 * 24 * 60 * 60 * 1000,
     url: `${ baseUrl }#test`,
   },
--- test.js
+++ test.js
@@ -10,7 +10,7 @@ const settings = require('./settings');
 /* Tests */
 /******************************************************************/
 
-const { timeout, url } = settings.test;
+const { keepalive, timeout, url } = settings.test;
 
 describe('visiting website', function() {
   this.timeout(0); // Disable timeouts
@@ -20,7 +20,7 @@ describe('visiting website', function() {
       const instance = await phantom.create();
       const page = await instance.createPage();
       await page.open(url);
-      setInterval(() => console.log('Keepalive', new Date()), 1000 * 30);
+      setInterval(() => console.log('Keepalive:', new Date()), keepalive);
       setTimeout(() => {
         instance.exit();
         done();
